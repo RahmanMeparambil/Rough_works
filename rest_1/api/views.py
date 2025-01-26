7f091f6d94883e4069bf8ef5a00adba00b0cf673
rahman <rahman821.ar@gmail.com>
2025-01-26 12:30:20 +0530

    Render data from Database
    Created models and serializers to render the data

--- rest_1/api/views.py
+++ rest_1/api/views.py
@@ -1,10 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import PersonSerializer, Person
 
 
 @api_view(['GET'])
 def home(request):
-    message  = {
-        'nums':[1,2,3]
-    }
-    return Response(message)
+    persons = Person.objects.all()
+    serializer = PersonSerializer(persons, many=True)
+    return Response(serializer.data)
